dd039145162e6ae067087eef149f35fbc12f3e60
quangnt2 <121183713+quangnt2@users.noreply.github.com>
2024-04-23 22:59:53 +0700

    Update ConnectDatabase.java

--- src/test/java/com/example/qung/Helper/ConnectDatabase.java
+++ src/test/java/com/example/qung/Helper/ConnectDatabase.java
@@ -5,7 +5,7 @@ import java.sql.*;
 public class ConnectDatabase {
     public static Connection getConnection() throws SQLException {
         String url = "jdbc:postgresql://103.138.113.158:5432/QLVB";
-        String username = "postgres";
+        String username = "/////";
         String password = "tringhia@12345";
         Connection conn = DriverManager.getConnection(url, username, password);
         return conn;
